13622489d9291afeead9cb006f9a637f8b4113bb
Юрий Савенков <yura-savenkov1@yandex.ru>
2020-12-26 15:02:13 +0300

    Footer Component

--- docs/pages/Home.js
+++ docs/pages/Home.js
@@ -1,8 +1,12 @@
 import React from 'react'
-import { JumbotronContainer } from '../containers/Jumbotron'
-
+import { JumbotronContainer } from '../containers/Jumbotron.js'
+import { FooterContainer } from '../containers/Footer.js'
+ 
 export default function Home() {
     return (
-        <JumbotronContainer />
+        <>
+            <JumbotronContainer />
+            <FooterContainer />
+        </>
     )
 }
