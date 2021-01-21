f8d486c6bfa48782250198df42651b267fa1d611
Юрий Савенков <yura-savenkov1@yandex.ru>
2021-01-21 09:41:36 +0300

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,6 @@ const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const OptimizeCssAssetWebpackPlugin = require('optimize-css-assets-webpack-plugin')
 const TerserWebpackPlugin = require('terser-webpack-plugin')
 const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer')
 
@@ -19,7 +17,6 @@ const optimization = () => {
 
   if (isProd) {
     config.minimizer = [
-      new OptimizeCssAssetWebpackPlugin(),
       new TerserWebpackPlugin()
     ]
   }
@@ -28,25 +25,6 @@ const optimization = () => {
 }
 
 const filename = ext => isDev ? `[name].${ext}` : `[name].[hash].${ext}`
-
-const cssLoaders = extra => {
-  const loaders = [
-    {
-      loader: MiniCssExtractPlugin.loader,
-      options: {
-        hmr: isDev,
-        reloadAll: true
-      },
-    },
-    'css-loader'
-  ]
-
-  if (extra) {
-    loaders.push(extra)
-  }
-
-  return loaders
-}
 
 const babelOptions = preset => {
   const opts = {
@@ -72,10 +50,6 @@ const jsLoaders = () => {
     options: babelOptions()
   }]
 
-  if (isDev) {
-    loaders.push('eslint-loader')
-  }
-
   return loaders
 }
 
@@ -93,10 +67,7 @@ const plugins = () => {
         from: path.resolve(__dirname, 'public/'),
         to: path.resolve(__dirname, 'dist/public')
       }
-    ]),
-    new MiniCssExtractPlugin({
-      filename: filename('css')
-    })
+    ])
   ]
 
   if (isProd) {
@@ -133,54 +104,14 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: cssLoaders()
-      },
-      {
-        test: /\.less$/,
-        use: cssLoaders('less-loader')
-      },
-      {
-        test: /\.s[ac]ss$/,
-        use: cssLoaders('sass-loader')
-      },
-      {
         test: /\.(png|jpg|svg|gif)$/,
         use: ['file-loader']
-      },
-      {
-        test: /\.(ttf|woff|woff2|eot)$/,
-        use: ['file-loader']
-      },
-      {
-        test: /\.xml$/,
-        use: ['xml-loader']
-      },
-      {
-        test: /\.csv$/,
-        use: ['csv-loader']
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: jsLoaders()
       },
-      {
-        test: /\.ts$/,
-        exclude: /node_modules/,
-        loader: {
-          loader: 'babel-loader',
-          options: babelOptions('@babel/preset-typescript')
-        }
-      },
-      {
-        test: /\.jsx$/,
-        exclude: /node_modules/,
-        loader: {
-          loader: 'babel-loader',
-          options: babelOptions('@babel/preset-react')
-        }
-      }
     ]
   }
 }
